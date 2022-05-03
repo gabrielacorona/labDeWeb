653d5d89fce8b324479eeaa67672978d190be30d
andresgenda <54486269+andresgenda@users.noreply.github.com>
2022-05-03 14:51:33 -0500

    Modified Foto Molde Card Component

--- src/components/moldes/FotosMoldes.js
+++ src/components/moldes/FotosMoldes.js
@@ -14,59 +14,58 @@ const mockProp = {
     fechaAdquisicion : "12-03-2022",
     descripcion : 'Este es un molde para hacer botellas',
     tipoColada : 'Muy',
-    encargado : "Andres"
+    encargado : "Andres",
+    mockImages : [
+        {
+          img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+          fechaPublicacion: 'Breakfast',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+          fechaPublicacion: 'Burger',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+          fechaPublicacion: 'Camera',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+          fechaPublicacion: 'Coffee',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+          fechaPublicacion: 'Hats',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+          fechaPublicacion: 'Honey',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+          fechaPublicacion: 'Basketball',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+          fechaPublicacion: 'Fern',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+          fechaPublicacion: 'Mushrooms',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+          fechaPublicacion: 'Tomato basil',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+          fechaPublicacion: 'Sea star',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+          fechaPublicacion: 'Bike',
+        },
+      ]
 }
-
-const mockImages = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      fechaPublicacion: 'Breakfast',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      fechaPublicacion: 'Burger',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      fechaPublicacion: 'Camera',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      fechaPublicacion: 'Coffee',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      fechaPublicacion: 'Hats',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      fechaPublicacion: 'Honey',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      fechaPublicacion: 'Basketball',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-      fechaPublicacion: 'Fern',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-      fechaPublicacion: 'Mushrooms',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-      fechaPublicacion: 'Tomato basil',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-      fechaPublicacion: 'Sea star',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-      fechaPublicacion: 'Bike',
-    },
-  ];
 
 export default function FotosMoldes() {
   return (
@@ -82,7 +81,6 @@ export default function FotosMoldes() {
         overflow: 'auto',
         }}
     >
-        {/* CONTAINER WITH DESCRIPCION AND FOTOS AND REPORTES BUTTONS */}
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={4}>
                 <Grid item xs={12} md={12} lg={12}>
@@ -96,11 +94,7 @@ export default function FotosMoldes() {
                 <Grid item xs={12} md={2} lg={2}>
                     <Typography>ID: {mockProp.id}</Typography>
                 </Grid>
-                {mockImages.map((item) => (
-                    <Grid item xs={12} md={6} lg={4}>
-                        <FotoMoldeCard image={item.img} fechaPublicacion={item.fechaPublicacion}/>
-                    </Grid>
-                ))}
+                <FotoMoldeCard imagenes={mockProp.mockImages}/>
             </Grid>
         </Container>
     </Box>
--- src/components/utils/FotoMoldeCard.js
+++ src/components/utils/FotoMoldeCard.js
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function FotoMoldeCard(props) {
   return (
-    <Card sx={{ maxWidth: "100%" }}>
-        <CardActionArea>
-            <CardMedia
-                component="img"
-                height="180"
-                image={props.image}
-                alt="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                    Fecha de publicación
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {props.fechaPublicacion}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-    </Card>
+    <React.Fragment>
+        {props.imagenes.map((item) => (
+            <Grid item xs={12} md={6} lg={4}>
+                <Card sx={{ maxWidth: "100%" }} onClick={(event) => {
+                console.log("Hola");
+                }}>
+                    <CardActionArea component={RouterLink} to="/DetalleFotoMolde">
+                        <CardMedia
+                            component="img"
+                            height="180"
+                            image={item.img}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                Fecha de publicación
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {item.fechaPublicacion}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        ))}
+    </React.Fragment>
   );
 }
