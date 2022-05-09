0dd096dc3d8f6f925d97710b58ec1cd63396e179
carooobregon <carooobregon@gmail.com>
2022-05-09 12:51:00 -0500

    working service

--- src/components/moldes/Moldes.js
+++ src/components/moldes/Moldes.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import axios from 'axios';
-
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Title from '../utils/Title'
 import DetailCard  from './DetailCard';
+import { getMoldes } from '../../services/moldes';
 
 const mockProp = {
     moldes: [
@@ -14,37 +14,18 @@ const mockProp = {
         {nombre: "molde3", id: "ghi"},
     ]
 }
-
-async function getMoldes() {
-    return fetch('/moldes', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJiNTgzMTBhLWU1ZjUtNGY4MS05ZmIwLWZlOTYxZjEyZmM2NSIsImZpcnN0TmFtZSI6InJpY2FyZG8iLCJsYXN0TmFtZSI6ImNvcm9uYSIsImVtYWlsIjoicmlja3lAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMDUkWldNc0NwZEsvbVNrajJJN1BVd1lBLk84YUpoNExla1UxTXFpandBZ0ZvdTBUZHU2SkI5VVciLCJjb21wYW55IjoiZ2FiYWNvIiwidGVsZXBob25lIjoiODE4MjY2MjIwMyIsInVzZXJQaWN0dXJlIjoicGVuZGllbnRlIiwiY29tcGFueVBpY3R1cmUiOiJwZW5kaWVudGUiLCJsYXN0UmVwb3J0RGF0ZSI6IiBheWVyICIsIm1lbWJlclNpbmNlIjoiYXllciAiLCJ1c2VyVHlwZSI6ImEiLCJpYXQiOjE2NTIxMTYzNjUsImV4cCI6MTY1MjExOTk2NX0.bDs5NobltWHg_mtGRnc-daOkN4eQoK8_BAu76QVImsY'
-      }
-    })
-    .then((response) => { 
-        return response.json().then((data) => {
-            console.log(data);
-            return data;
-        }).catch((err) => {
-            console.log(err);
-        }) 
-    });
-}
   
 export default function Moldes() {
     const [moldes, setMoldes] = useState([]);
 
-    const fetchData = useCallback(async () => {
+    const fetchMoldeData = useCallback(async () => {
         const data = await getMoldes()
       
         setMoldes(data);
       }, [])
 
     useEffect(() => {
-        fetchData()
-        // make sure to catch any error
+        fetchMoldeData()
         .catch(console.error);;    
     }, []);
 
